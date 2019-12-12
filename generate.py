9d1a44db966e060707640fecfcc51cd5f338708c
Andrew Peterson <andrew_peterson1@brown.edu>
2019-12-12 14:21:49 -0500

    finalized code

--- generate.py
+++ generate.py
@@ -13,20 +13,22 @@ CAPTCHA_MIN_LEN = 4
 CAPTCHA_MAX_LEN = 8
 
 def main():
-    #os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    #font_files = glob.glob('easy_fonts/*.ttf')
-    generator = ImageCaptcha(fonts=['font/Cantarell-Regular.ttf'], font_sizes=[42]) #width=160, height=60, font_sizes=(42,50,56)
+    #create a captcha generator
+    generator = ImageCaptcha(fonts=['font/Cantarell-Regular.ttf'], font_sizes=[42]) #width=160, height=60
 
+    #create the directories for the training and testing images
     if not os.path.exists('train_imgs'):
         os.makedirs('train_imgs')
     if not os.path.exists('test_imgs'):
         os.makedirs('test_imgs')
 
+    #generate training captchas
     for x in range(NUM_TRAIN):
         str = ''.join([random.choice(string.ascii_uppercase + string.digits) \
                        for n in range(np.random.randint(CAPTCHA_MIN_LEN,CAPTCHA_MAX_LEN))])
         generator.write(str, 'train_imgs/{}.png'.format(str))
 
+    #generate testing captchas
     for x in range(NUM_TEST):
         str = ''.join([random.choice(string.ascii_uppercase + string.digits) \
                        for n in range(np.random.randint(CAPTCHA_MIN_LEN,CAPTCHA_MAX_LEN))])
--- preprocess.py
+++ preprocess.py
@@ -5,12 +5,8 @@ import cv2
 import imutils
 import string
 
-import pdb
-
-THRESHOLD = 200
-OUTPUT_DIRECTORY = 'letters'
-
 def get_data():
+	#get the training data
 	os.chdir(os.path.dirname(os.path.realpath(__file__)))
 	train_pngs = glob.glob('train_imgs/*.png')
 	train_examples = []
@@ -20,6 +16,7 @@ def get_data():
 		train_examples.append(img)
 		train_labels.append(png)
 
+	#process the images and get the strings for the training labels
 	train_examples = np.array(train_examples).astype(np.float32)
 	train_examples /= 255.0
 	train_examples = 1-train_examples
@@ -27,6 +24,7 @@ def get_data():
 	train_lengths = np.array([len(str) for str in train_labels])
 	max_len = max(train_lengths)
 
+	#pad the training labels
 	alphanumeric = string.digits + string.ascii_uppercase
 	train_labels = [alphanumeric.find(char) for label in train_labels for char in label]
 	to_fill = np.zeros([train_examples.shape[0], max_len], dtype=int)
@@ -34,13 +32,14 @@ def get_data():
 	row = 0
 	while i < len(train_labels):
 		lst = train_labels[i:i+train_lengths[row]]
-		lst.extend((max_len - train_lengths[row])*[0])
+		lst.extend((max_len - train_lengths[row])*[-1])
 		to_fill[row] = np.array(lst)
 		i += train_lengths[row]
 		row += 1
 
 	train_labels = to_fill
 
+	#get the testing data
 	test_pngs = glob.glob('test_imgs/*.png')
 	test_examples = []
 	test_labels = []
@@ -49,6 +48,7 @@ def get_data():
 		test_examples.append(img)
 		test_labels.append(png)
 
+	#process the images and get the strings for the testing labels
 	test_examples = np.array(test_examples).astype(np.float32)
 	test_examples /= 255.0
 	test_examples = 1-test_examples
@@ -56,13 +56,14 @@ def get_data():
 	test_lengths = np.array([len(str) for str in test_labels])
 	max_len = max(test_lengths)
 
+	#pad the testing labels
 	test_labels = [alphanumeric.find(char) for label in test_labels for char in label]
 	to_fill = np.zeros([test_examples.shape[0], max_len], dtype=int)
 	i = 0
 	row = 0
 	while i < len(test_labels):
 		lst = test_labels[i:i+test_lengths[row]]
-		lst.extend((max_len - test_lengths[row])*[0])
+		lst.extend((max_len - test_lengths[row])*[-1])
 		to_fill[row] = np.array(lst)
 		i += test_lengths[row]
 		row += 1
